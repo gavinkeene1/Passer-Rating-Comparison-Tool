82100e0ac2861400035b4a254c73d264e00b5b68
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2022-11-15 15:08:23 +0800

    Update yarn.lock and React Import Statements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import ShowBookcase from './ShowBookcase';
 import SearchBooks from './SearchBooks';
 import * as BooksAPI from './BooksAPI';
