c3aa18e3a50e79e5227175b6147f70a6468d99bb
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2022-11-15 16:10:56 +0800

    Remove Unused Code / Import Statements - PasserRatingInput.js

--- src/PasserRatingInput.js
+++ src/PasserRatingInput.js
@@ -1,32 +1,5 @@
 import * as React from 'react';
-import { useState, useCallback } from 'react';
-import { Link } from 'react-router-dom';
-
-/*class PasserRatingInput extends React.Component {
-    render() {
-      const [completions, setCompletions] = useState(0);
-        return(
-            <div className="search-books">
-            <div className="search-books-bar">
-              <Link to='/' className='close-search'>Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder={`Passer Rating Input`}/>
-              </div>
-              <div>
-      <p>You clicked {completions} times</p>
-      <button onClick={() => setCompletions(completions + 1)}>
-        Click me
-      </button>
-    </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-          
-        )
-    }
-}*/
+import { useState } from 'react';
 
 function PasserRatingInput() {
   const [completions, setCompletions] = useState(0);
