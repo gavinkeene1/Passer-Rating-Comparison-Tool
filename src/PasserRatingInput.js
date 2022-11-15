e939fb10bb19431f0d61fd4e35372eeb71235135
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2022-11-15 15:24:57 +0800

    Create HTML Inputs for Passer Rating Values

--- src/PasserRatingInput.js
+++ src/PasserRatingInput.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class PasserRatingInput extends React.Component {
+/*class PasserRatingInput extends React.Component {
     render() {
+      const [completions, setCompletions] = useState(0);
         return(
             <div className="search-books">
             <div className="search-books-bar">
@@ -11,13 +12,39 @@ class PasserRatingInput extends React.Component {
               <div className="search-books-input-wrapper">
                 <input type="text" placeholder={`Passer Rating Input`}/>
               </div>
+              <div>
+      <p>You clicked {completions} times</p>
+      <button onClick={() => setCompletions(completions + 1)}>
+        Click me
+      </button>
+    </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid"></ol>
             </div>
           </div>
+          
         )
     }
+}*/
+
+function PasserRatingInput() {
+  const [completions, setCompletions] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {completions} times</p>
+      <button onClick={() => setCompletions(completions + 1)}>
+        Click me
+      </button>
+      <input type="text" id="fname" name="fname" placeholder='comp' style={{width: 100, margin: 4}} />
+      <input type="text" id="fname" name="fname" placeholder='att' style={{width: 100, margin: 4}} />
+      <input type="text" id="fname" name="fname" placeholder='yards' style={{width: 100, margin: 4}} />
+      <input type="text" id="fname" name="fname" placeholder='tds' style={{width: 100, margin: 4}} />
+      <input type="text" id="fname" name="fname" placeholder='ints' style={{width: 100, margin: 4}} /><br /><br></br>
+    </div>
+  );
 }
 
 export default PasserRatingInput;
+//export default PasserRatingInput;
