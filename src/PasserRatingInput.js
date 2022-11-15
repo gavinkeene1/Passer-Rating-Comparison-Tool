8beace8267adef43488c11da264fdde507cbf8f9
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2022-11-15 16:45:08 +0800

    Create `getPasserRating` - PasserRatingInput.js

--- src/PasserRatingInput.js
+++ src/PasserRatingInput.js
@@ -1,6 +1,28 @@
 import * as React from 'react';
 import { useState } from 'react';
 
+const clamp =  require('lodash/clamp');
+const round = require('lodash/round');
+
+/** Get Passer Rating
+* @Docs https://en.wikipedia.org/wiki/Passer_rating#NFL_and_CFL_formula
+* @Dependencies: lodash (clamp, round) */
+const getPasserRating = (
+  attempts,
+  completions,
+  yards,
+  touchdowns,
+  interceptions
+) => {
+  if (attempts === 0) return 0
+  const a = clamp((completions / attempts - 0.3) * 5, 0, 2.375)
+  const b = clamp((yards / attempts - 3) * 0.25, 0, 2.375)
+  const c = clamp((touchdowns / attempts) * 20, 0, 2.375)
+  const d = clamp(2.375 - (interceptions / attempts) * 25, 0, 2.375)
+
+  return round(((a + b + c + d) / 6) * 100, 1)
+};
+
 const PasserRatingInput = () => {
   const [completions, setCompletions] = useState(0);
   const [attempts, setAttempts] = useState(0);
@@ -9,12 +31,14 @@ const PasserRatingInput = () => {
   const [interceptions, setInterceptions] = useState(0);
 
   const setValue = (event) => {
-    const value = Number(event.target.value);
+    const value = Number(event.target.value ? event.target.value : 0);
     event.target.id === 'completions' && setCompletions(value);
     event.target.id === 'attempts' && setAttempts(value);
     event.target.id === 'yards' && setYards(value);
     event.target.id === 'touchdowns' && setTouchdowns(value);
     event.target.id === 'interceptions' && setInterceptions(value);
+    console.log(`value changed to ${value}`);
+    console.log(getPasserRating(completions, attempts, yards, touchdowns, interceptions));
   };
 
   return (
